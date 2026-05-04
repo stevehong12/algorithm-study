628a86b5e89d1e530138ada37b78cd60b69ab8fe
stevehong12 <stevehong124@gmail.com>
2026-05-04 11:19:55 +0900

    Day02 풀이 추가 - 숨어있는 숫자, 순서쌍, 개미군단, 모스부호

---
+++ 알고리즘 기초/Day02/숨어있는 숫자의 덧셈 (1).py
@@ -0,0 +1,9 @@
+# 문자열 my_string이 매개변수로 주어집니다.
+# my_string안의 모든 자연수들의 합을 return하도록
+# solution 함수를 완성해주세요.
+def solution(my_string):
+    answer = 0
+    for ch in my_string:
+        if ch.isdigit():
+            answer += int(ch)
+    return answer
